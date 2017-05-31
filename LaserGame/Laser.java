38ab212ab20b3eff259973e7923af3dd3ea92912
jjjdjj <JasonLi1000@hotmail.com>
2017-05-31 13:15:16 -0400

    Update Laser.java

--- LaserGame/Laser.java
+++ LaserGame/Laser.java
@@ -9,15 +9,35 @@ import greenfoot.*;
 public abstract class Laser extends Actor
 {
     MouseInfo mouse;
+    //Have a type of beam selected
+    protected int beamSelected = 1;
     protected int maxSquares = 10;
+    /**
+     * Constructs laser with defaults
+     */
+    public Laser(){
+        
+    }
+    /**
+     * Constructs laser based on beam
+     */
+    public Laser(int b){
+        beamSelected = b;
+    }
     /**
      * Act - do whatever the Laser wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        check();
+        actMethods();
     }    
+    /**
+     * Put all act methods here to easily deal with inheritance
+     */
+    protected void actMethods(){
+        check();
+    }
     protected void check(){
         if(leftDown()){
             //If it's the first one, you can click anywhere on the first rank
@@ -25,20 +45,26 @@ public abstract class Laser extends Actor
                 Tile mouseGrid = getMouseGrid();
                 if(mouseGrid.getMidY() == 600){
                     mouseGrid.highlight();
+                    Grid.setLastHighlighted(mouseGrid);
                     PlayWorld.getGrid().showGrid();
                 }
             }
             //If it's not, only allow sides to prevent diagonal and skipping (TOO MUCH TIME BETWEEN ACTS!!)
             //And also only if the number already highlighted is less than max
+            //And also only if the last tile is one next to it
             else if(Grid.getNumHighlighted() < maxSquares){
                 Tile mouseGrid = getMouseGrid();
-                if(Grid.isSideHighlighted(mouseGrid)){
+                if(Grid.isSideLastHighlighted(mouseGrid)){
+                    if(!mouseGrid.isHighlighted()){
+                        Grid.setLastHighlighted(mouseGrid);
+                    }
                     mouseGrid.highlight();
                     PlayWorld.getGrid().showGrid();
                 }
             }
         }
         if(Greenfoot.mouseClicked(null)){
+            fireBeam();
             Grid.clearTiles();
             PlayWorld.getGrid().hideGrid();
         }
@@ -86,4 +112,26 @@ public abstract class Laser extends Actor
             maxSquares = max;
         }
     }
+    /**
+     * Spawns the beam on the tile
+     */
+    public void fire(Tile t){
+        getWorld().addObject(new Beam(50), t.getX(), t.getY());
+        //Note: disappears in beam class
+    }
+    /**
+     * Fires the entire beam
+     */
+    public void fireBeam(){
+        Tile[] highlighted = Grid.getHighlighted();
+        for(int i = 0;i<highlighted.length;i++){
+            //If there is an obstacle
+            if(Grid.isObstacle(highlighted[i])){
+            }
+            else{
+                fire(highlighted[i]);
+            }
+        }
+    }
 }
+
